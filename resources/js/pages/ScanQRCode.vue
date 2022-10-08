6119bc16a69b7601e541c269913a0bebe3f3f1c5
dbyjar <fjralhakim@gmail.com>
2022-10-08 21:27:36 +0700

    [added] scan qr package

---
+++ resources/js/pages/ScanQRCode.vue
@@ -0,0 +1,22 @@
+<template>
+  <StreamBarcodeReader
+    @decode="onDecode"
+    @loaded="onLoaded"
+  ></StreamBarcodeReader>
+</template>
+
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+export default {
+  components: { StreamBarcodeReader },
+  methods: {
+    onDecode(val) {
+      console.log("decode: ", val)
+    },
+    onLoaded(val) {
+      console.log("laoded: ", val)
+    },
+  }
+}
+</script>
