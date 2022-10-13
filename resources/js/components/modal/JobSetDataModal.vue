a0d4317bcac6acdc670e084aa8f5b7e91ba98e8e
dbyjar <fjralhakim@gmail.com>
2022-10-13 15:56:55 +0700

    [update] job setdata

---
+++ resources/js/components/modal/JobSetDataModal.vue
@@ -0,0 +1,13 @@
+<template>
+  <BaseModal ref="bsModal">
+    <template #title>Title</template>
+  </BaseModal>
+</template>
+
+<script>
+import { bsModalComponentOptions } from '@/mixins'
+
+export default {
+  mixins: [bsModalComponentOptions("bsModal")]
+}
+</script>
--- resources/js/pages/Jobs.vue
+++ resources/js/pages/Jobs.vue
@@ -1,39 +1,44 @@
 <template>
   <div>
-    <Tools @click="$router.push({
-      name: 'jobs-create'
-    })" />
-    <div class="row">
-      <div class="col-lg-12">
-        <Box>
-          <BaseTable
-            :config="{
-              namespace: 'job',
-            }"
-            :columns="columns"
-            v-slot="{ rows }"
-          >
-            <base-table-row v-for="row in rows" :key="row.id">
-              <base-table-column>{{ formatDate(row.created_at) }}</base-table-column>
-              <base-table-column>{{ row.name }}</base-table-column>
-              <base-table-column class="text-end" overflow-visible>
-                <action-tool
-                  @edit="edit(row.id)"
-                  @remove="remove(row.id)"
-                  withoutDetail
-                ></action-tool>
-              </base-table-column>
-            </base-table-row>
-          </BaseTable>
-        </Box>
-      </div>
-    </div>
+    <Tools @click="showModalAdd = true" />
+    <!-- <Tools withoutDefaultButton>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal">
+        Launch demo modal
+      </button>
+    </Tools> -->
+    <Box>
+      <BaseTable
+        :config="{
+          namespace: 'job',
+        }"
+        :columns="columns"
+        v-slot="{ rows }"
+      >
+        <base-table-row v-for="row in rows" :key="row.id">
+          <base-table-column>{{ formatDate(row.created_at) }}</base-table-column>
+          <base-table-column>{{ row.name }}</base-table-column>
+          <base-table-column class="text-end" overflow-visible>
+            <action-tool
+              @edit="edit(row.id)"
+              @remove="remove(row.id)"
+              withoutDetail
+            ></action-tool>
+          </base-table-column>
+        </base-table-row>
+      </BaseTable>
+    </Box>
+
+    <job-set-data-modal :value.sync="showModalAdd" />
   </div>
 </template>
 
 <script>
+import JobSetDataModal from '@components/modal/JobSetDataModal.vue'
+
 export default {
+  components: { JobSetDataModal },
   data: () => ({
+    showModalAdd: false,
     columns: [
       {
         name: "created at",
@@ -45,5 +50,12 @@ export default {
       },
     ]
   }),
+  methods: {
+    onShowModalAddClick() {
+      this.showModalAdd = true
+      this.$forceUpdate()
+      // this.$router.push({ name: 'jobs-create' })
+    }
+  }
 }
 </script>
