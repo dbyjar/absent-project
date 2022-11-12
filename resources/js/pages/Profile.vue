44d5f6efa8fdf68062513e0ca613f9736b9f3957
Fajar Al Hakim <fjralhakim@gmail.com>
2022-11-12 18:48:54 +0700

    [update] profile page

--- resources/js/pages/Profile.vue
+++ resources/js/pages/Profile.vue
@@ -1,43 +1,109 @@
 <template>
-  <Box>
-    <form class="form-horizontal form-material mx-2">
-      <div class="form-group">
-        <label>Full Name</label>
-        <input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+  <div>
+    <Box>
+      <table>
+        <tr>
+          <th>Nama</th>
+          <th>:</th>
+          <td>{{ user.name }}</td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <th>:</th>
+          <td>{{ user.email }}</td>
+        </tr>
+        <tr>
+          <th>ID</th>
+          <th>:</th>
+          <td>{{ user.nik }}</td>
+        </tr>
+        <tr>
+          <th>Birthdate</th>
+          <th>:</th>
+          <td>{{ user.birthdate }}</td>
+        </tr>
+        <tr>
+          <th>phone</th>
+          <th>:</th>
+          <td>{{ user.phone }}</td>
+        </tr>
+        <tr>
+          <th>Role</th>
+          <th>:</th>
+          <td>{{ user.user_role.name }}</td>
+        </tr>
+        <tr>
+          <th>Job</th>
+          <th>:</th>
+          <td>{{ user.job.name }}</td>
+        </tr>
+        <tr>
+          <th>Address</th>
+          <th>:</th>
+          <td>{{ user.address }}</td>
+        </tr>
+      </table>
+    </Box>
+    <div class="row">
+      <div class="col-lg-12">
+        <Box :title="'Attendance'">
+          <BaseTable
+            ref="table"
+            :config="{
+              namespace: 'attendance',
+              params: {
+                employee_id: $root.auth.id,
+              },
+            }"
+            :columns="columns"
+            v-slot="{ rows }"
+            withoutOptionColumn
+          >
+            <base-table-row v-for="row in rows" :key="row.id">
+              <base-table-column>{{ row.punch_in }}</base-table-column>
+              <base-table-column>{{
+                row.shift_and_salary?.name
+              }}</base-table-column>
+              <base-table-column>{{
+                row.shift_and_salary?.price
+              }}</base-table-column>
+            </base-table-row>
+          </BaseTable>
+        </Box>
       </div>
-      <div class="form-group">
-        <label>Email</label>
-        <input type="email" class="form-control form-control-line">
-      </div>
-      <div class="form-group">
-        <label>Password</label>
-        <input type="password" class="form-control form-control-line">
-      </div>
-      <div class="form-group">
-        <label>Phone No</label>
-        <input type="text" class="form-control form-control-line">
-      </div>
-      <div class="form-group">
-        <label>Message</label>
-        <textarea rows="5" class="form-control form-control-line"></textarea>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-12">Select Country</label>
-        <div class="col-sm-12">
-          <select class="form-select shadow-none form-control-line">
-            <option>London</option>
-            <option>India</option>
-            <option>Usa</option>
-            <option>Canada</option>
-            <option>Thailand</option>
-          </select>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-12">
-          <button class="btn btn-success text-white">Update Profile</button>
-        </div>
-      </div>
-  </form>
-  </Box>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    user: {},
+    columns: [
+      {
+        name: "punch in",
+        class: "",
+      },
+      {
+        name: "shift",
+        class: "",
+      },
+      {
+        name: "price",
+        class: "",
+      },
+    ],
+  }),
+  async mounted() {
+    const { results } =
+      (
+        await axios.get(
+          `/api/employee/${this.$root.auth.id}`,
+          this.$root.headersRequestAPI
+        )
+      ).data ?? {};
+
+    this.user = results.data;
+  },
+};
+</script>
